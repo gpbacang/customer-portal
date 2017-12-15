5533311558422c0c9bd3face093ec2c122d07468
gpbacang <gpbacang@Geraldines-MacBook-Pro.local>
2017-12-15 10:05:31 +0400

    Initial commit

---
+++ src/app/activities/activities.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { BaseChartDirective } from 'ng2-charts';
+
+@Component({
+  selector: 'app-activities',
+  templateUrl: './activities.component.html',
+  styleUrls: ['./activities.component.scss']
+})
+export class ActivitiesComponent implements OnInit {
+  pieChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
+  pieChartData: number[] = [300, 500, 100];
+  pieChartType = 'pie';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ActivitiesComponent } from './activities/activities.component';
+import { ActivitiesListComponent } from './activities/activities-list/activities-list.component';
+import { ActivityCardComponent } from './activities/activity-card/activity-card.component';
+
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ActivitiesComponent,
+    ActivitiesListComponent,
+    ActivityCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
